40e09ec0b04cd4fe2a6078e742e11f007990e86a
EvgrafovAlexander <35798672+EvgrafovAlexander@users.noreply.github.com>
2023-03-02 17:29:49 +0500

    feat(web): change reader detail

--- web/data/percent_data.py
+++ web/data/percent_data.py
@@ -3,4 +3,4 @@ from web.readers import audiences_reader
 
 async def get_percent_data(first_condition: str, second_condition: str):
     data = await audiences_reader.get_percent_data(first_condition, second_condition)
-    return data
+    return datahh
