64528338ade9dcee4a953cac0b91ccccaf9b85cc
EvgrafovAlexander <35798672+EvgrafovAlexander@users.noreply.github.com>
2023-03-03 11:01:11 +0500

    exclude async_test

--- core/common/db_common.py
+++ core/common/db_common.py
@@ -1,5 +1,4 @@
 # stdlib
-import asyncio
 import collections
 import itertools
 import re
@@ -77,13 +76,3 @@ async def execute_multiple(sql_params_list: List[Tuple[str, List[dict]]]) -> int
                 values = prepared_query[1]
                 await conn.executemany(query, values)
     return rowcount
-
-
-def async_test(func):
-    def wrapper(*args, **kwargs):
-        coro = asyncio.coroutine(func)
-        future = coro(*args, **kwargs)
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(future)
-
-    return wrapper
--- core/readers/tests/test_audiences_reader.py
+++ core/readers/tests/test_audiences_reader.py
@@ -4,13 +4,11 @@ import unittest
 
 # project
 from common import db_common
-from common.db_common import async_test
 from readers import audiences_reader
 
 
 class TestAudiencesReader(unittest.TestCase):
     @classmethod
-    @async_test
     async def setUpClass(cls) -> None:
         cls.date = datetime.date(1900, 1, 1)
         insert_sql = """
@@ -80,42 +78,35 @@ class TestAudiencesReader(unittest.TestCase):
         await db_common.execute_multiple([(insert_sql, cls.insert_data)])
 
     @classmethod
-    @async_test
     async def tearDownClass(cls) -> None:
         delete_sql = "delete from audiences au where au.date = :date"
         await db_common.execute_multiple([(delete_sql, cls.insert_data)])
 
-    @async_test
     async def test_get_percent(self):
         expected = [{"percent": 75.0}]
         result = await audiences_reader.get_percent_data("Age = 100", "Age = 101")
         self.assertListEqual(result, expected)
 
-    @async_test
     async def test_get_percent_with_identical_audiences(self):
         expected = [{"percent": 100.0}]
         result = await audiences_reader.get_percent_data("Age = 100", "Age = 100")
         self.assertListEqual(result, expected)
 
-    @async_test
     async def test_get_percent_with_no_intersections(self):
         expected = [{"percent": 0.0}]
         result = await audiences_reader.get_percent_data("Age = 100", "Age = 200")
         self.assertListEqual(result, expected)
 
-    @async_test
     async def test_get_percent_where_2au_in_1au(self):
         expected = [{"percent": 25.0}]
         result = await audiences_reader.get_percent_data("Age in (102, 103)", "Age = 102")
         self.assertListEqual(result, expected)
 
-    @async_test
     async def test_get_percent_where_1au_in_2au(self):
         expected = [{"percent": 100.0}]
         result = await audiences_reader.get_percent_data("Age = 102", "Age in (102, 103)")
         self.assertListEqual(result, expected)
 
-    @async_test
     async def test_get_percent_not_exist_group(self):
         expected = [{"percent": 100.0}]
         result = await audiences_reader.get_percent_data("Age = 130", "Age = 140")
