a95be63a1d4d9576c6e5754dceb13c593a7ce1ec
EvgrafovAlexander <35798672+EvgrafovAlexander@users.noreply.github.com>
2023-03-04 09:36:34 +0500

    add envs

--- core/common/postgres_connector.py
+++ core/common/postgres_connector.py
@@ -1,6 +1,9 @@
 # thirdparty
 import asyncpg
 
+# project
+from common import configuration as cfg
+
 _CONNECTION_POOL = None
 
 
@@ -10,13 +13,13 @@ async def get_pool():
 
     if not _CONNECTION_POOL:
         _CONNECTION_POOL = await asyncpg.create_pool(
-            user="postgres",
-            password="postgres",
-            database="postgres",
-            host="database",
-            port=5432,
-            max_inactive_connection_lifetime=200,
-            min_size=1,
-            max_size=50,
+            user=cfg.ConnectionConfig.user,
+            password=cfg.ConnectionConfig.password,
+            database=cfg.ConnectionConfig.database,
+            host=cfg.ConnectionConfig.host,
+            port=cfg.ConnectionConfig.port,
+            max_inactive_connection_lifetime=cfg.PgPoolSettings.MAX_INACTIVE_CONNECTION_LIFETIME,
+            min_size=cfg.PgPoolSettings.MIN_POOL_SIZE,
+            max_size=cfg.PgPoolSettings.MAX_POOL_SIZE,
         )
     return _CONNECTION_POOL
